6e3961f4d8a035b09f4f8481f42d63db80854126
Zemlyanskiy <nikita.nikita.000035@gmail.com>
2021-05-20 11:00:50 +0300

    Get metaData

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,8 +1,9 @@
+import os
 import csv
 import requests
 from bs4 import BeautifulSoup
 import re
-#from concurrent.futures import ThreadPoolExecutor as PoolExecutor
+#from concurrent.futures import ProcessPoolExecutor as PoolExecutor
 import time
 from requests_html import HTMLSession
 
@@ -15,12 +16,15 @@ def scrape_data(url):
     soup = BeautifulSoup(response.content, 'html.parser')
     fileName = re.sub('https://www.iasg.com/en-us/groups/group/', '', url)
     fileName = re.sub('/', '.', fileName)
-    with open(fileName + '.csv', 'w', newline='', encoding='utf-8') as csv_file:
+    with open('..//data//' + fileName + '.csv', 'w', newline='', encoding='utf-8') as csv_file:
         writer = csv.writer(csv_file)
         session = HTMLSession()
         r = session.get(url)
         r.html.render(timeout=30, sleep=10)
         tableCalendar = r.html.find('table', first=True)
+        if tableCalendar is None:
+            os.remove('..//data//' + fileName + '.csv')
+            return
         strList = tableCalendar.text.split('\n')
         str=''
         for index in range(0, len(strList)):
@@ -33,7 +37,7 @@ def scrape_data(url):
             writer.writerow([s])
         tables = soup.find_all('table')
         for table in tables:
-            if table != soup.find('table', class_='performance-table responsive'):
+            if table != soup.find('table', class_='performance-table responsive') and table not in soup.find_all('table', class_='minimal'):
                 body = table.find('tbody')
                 head = table.find('thead')
                 if head is not None:
@@ -53,17 +57,47 @@ def scrape_data(url):
                     for col in dinColumns:
                         str+=col.text.strip()+' '
                     writer.writerow([str])
+        strategy = soup.find_all('div', class_='span-1-1-2-desktop performance-summary')[1]
+        if strategy is not None:
+            dtStrategy = strategy.find_all('dt')
+            ddStrategy = strategy.find_all('dd')
+            if dtStrategy is not None and ddStrategy is not None:
+                for dt, dd in zip(dtStrategy, ddStrategy):
+                    a = dt.find('a')
+                    writer.writerow([a.text.strip()+' '+dd.text.strip()])
+        composition = soup.find_all('div', class_='span-1-2-2-desktop performance-summary')[1]
+        if composition is not None:
+            dtComposition = composition.find_all('dt')
+            ddComposition = composition.find_all('dd')
+            if dtComposition is not None and ddComposition is not None:
+                for dt, dd in zip(dtComposition, ddComposition):
+                    a = dt.find('a')
+                    writer.writerow([a.text.strip()+' '+dd.text.strip()])
+        risks = soup.find('div', class_='span-1-3-4-desktop performance-summary')
+        if risks is not None:
+            dtRisks = risks.find_all('dt')
+            ddRisks = risks.find_all('dd')
+            if dtRisks is not None and ddRisks is not None:
+                for dt, dd in zip(dtRisks, ddRisks):
+                    writer.writerow([dt.text.strip()+' '+dd.text.strip()])
+        asserts = soup.find('div', class_='span-1-4-4-desktop performance-summary')
+        if asserts is not None:
+            dtAsserts = asserts.find_all('dt')
+            ddAsserts = asserts.find_all('dd')
+            if dtAsserts is not None and ddAsserts is not None:
+                for dt, dd in zip(dtAsserts, ddAsserts):
+                    writer.writerow([dt.text.strip()+' '+dd.text.strip()])
 
 if __name__=="__main__":
     start_time = time.time()
     with open('links.txt', 'r') as f:
         urls = f.read().splitlines()
-        #with PoolExecutor(max_workers=4) as executor:
+        #with PoolExecutor(max_workers=16) as executor:
         #    for index, value in enumerate(executor.map(scrape_data, urls)):
-        #        print(index+' '+value)
+        #        print(f'{index} {urls[index]}')
         #        pass
-        for index in range(365,len(urls)):
-            print(f'{index} {urls[index]}')
+        for index in range(235, len(urls)):
+            print(f'{index} {urls[index]}') 
             scrape_data(urls[index])
     print("--- %s seconds ---" % (time.time() - start_time))
 
