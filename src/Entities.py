c09d96113c4a49dd8762bf843730cf8072b17d8e
Zemlyanskiy Nikita <nikita.nikita.000035@gmail.com>
2021-05-25 02:14:13 +0300

    baseRealization

---
+++ src/Entities.py
@@ -0,0 +1,28 @@
+class Portfolio:
+    def __init__(self, capital=None, sharpeRatio=None, risk = None, perfomance = None):
+        self.Funds = []
+        self.Capital = capital
+        self.SharpeRatio = sharpeRatio
+        self.Risk = risk
+        self.Perfomance = perfomance
+
+class HedgeFund:
+    def __init__(self, name = None, sharpeRatioData=None, minInvest = None, weight = None):
+        self.Name = name
+        self.SharpeRatioData = sharpeRatioData
+        self.MinInvest = minInvest
+        self.Statistics= []
+        self.Weight = weight
+
+class Statistics:
+    def __init__(self, year=None, ytd=None, dd=None):
+        self.Year = year
+        self.Ytd = ytd
+        self.Dd = dd
+        self.MonthPerform = []
+
+class MonthPerform:
+    def __init__(self, month=None, year=None, perfomance=None):
+        self.Month = month
+        self.Perfomance = perfomance
+        self.Year = year
