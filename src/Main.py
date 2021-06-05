8dd5dc851173f6dfc278a118dac5e26b2ea36084
Zemlyanskiy Nikita <nikita.nikita.000035@gmail.com>
2021-06-06 02:25:40 +0300

    Modify calculating sharpe ratio

--- src/Main.py
+++ src/Main.py
@@ -6,7 +6,7 @@ from src.Entities import Portfolio, HedgeFund
 import numpy as np
 import matplotlib.pyplot as plt
 
-def CheckWeights(portfolio, fund, delta, capit):
+def CheckWeights(portfolio, fund, delta):
     alpha = 0.0
     capital = float(portfolio.Capital)
     newPorts = []
@@ -22,21 +22,25 @@ def CheckWeights(portfolio, fund, delta, capit):
             n = HedgeFund(f.Name, f.SharpeRatioData, f.MinInvest, float(f.Weight))
             n.Statistics = f.Statistics
             newPort.Funds.append(n)
-        newPort.Capital = portfolio.Capital
         alpha += delta
-        capital -= newFund.MinInvest * alpha
+        capital -= newFund.MinInvest * (1 - alpha)
+        newPort.Capital = float(capital)
         for f in newPort.Funds:
             f.Weight *= alpha
         newFund.Weight = float(1 - alpha)
         newPort.Funds.append(newFund)
-        CalculateSharpe(newPort, capit)
+        CalculateSharpe(newPort)
         newPorts.append(newPort)
 
     return newPorts
 
+def isNotInList(funds, name):
+    for fund in funds:
+        if fund.Name == name:
+            return False
+    return True
+
 def Main():
-    #capital = int(sys.argv[1])
-    iterations = 1
     portfolio = Portfolio()
     portfolio.Funds = []
     portfolio.Capital = 1000000
@@ -48,45 +52,48 @@ def Main():
             h.Statistics = h.Statistics[:2]
             get_list.append(h)
     hedgeFundList = get_list
+    #iterations = len(get_list)
+    iterations = 10
+    delta = 0.1
     portfolio.Funds.append(max(hedgeFundList, key=attrgetter('SharpeRatioData')))
     portfolio.Funds[0].Weight = 1.0
-    capit = float(portfolio.Capital)
     portfolio.Capital -= portfolio.Funds[0].MinInvest
-    CalculateSharpe(portfolio, capit)
-    ports=[]
-    delta = 0.1
+    CalculateSharpe(portfolio)
+    ports = []
+    portfolio.Index = 0
+    ports.append(portfolio)
     for i in range(0, iterations):
         for index, fund in enumerate(hedgeFundList):
-            if portfolio.Funds.count(fund) == 0:
-                portfolies = CheckWeights(portfolio, fund, delta, capit)
+            if isNotInList(portfolio.Funds, fund.Name):
+                portfolies = CheckWeights(portfolio, fund, delta)
+                if len(portfolies) == 0:
+                    continue
                 newPortfolio = max(portfolies, key=attrgetter('SharpeRatio'))
                 if newPortfolio.SharpeRatio > portfolio.SharpeRatio:
                     portfolio = newPortfolio
-                    newPortfolio.Index = i
-                    ports.append(newPortfolio)
+        portfolio.Index = i + 1
+        ports.append(portfolio)
     rt = []
     for i in range(0, iterations):
         if i < len(ports):
-            print('SharpeRatio: '+str(ports[i].SharpeRatio))
             rt.append(ports[i].SharpeRatio)
         else:
             rt.append(0.0)
-    rt.sort()
     plt.plot(np.arange(iterations), rt)
     plt.xlabel('Iterations')
     plt.ylabel('SharpeRatio')
+    print('Remaining Capital: '+str(portfolio.Capital))
+    print('SharpeRatio: '+str(portfolio.SharpeRatio))
+    print('Risk: '+str(portfolio.Risk))
+    print('Perfomance: '+str(portfolio.Perfomance) + '\n')
+    wSum = 0.0
+    for fund in portfolio.Funds:
+        print('FundName: '+fund.Name)
+        print('SharpeRatioData: '+str(fund.SharpeRatioData))
+        print('Weight: '+str(fund.Weight))
+        wSum += fund.Weight
+    print(str(wSum))
     plt.show()
-    #print('Capital: '+str(portfolio.Capital))
-    #print('SharpeRatio: '+str(portfolio.SharpeRatio))
-    #print('Risk: '+str(portfolio.Risk))
-    #print('Perfomance: '+str(portfolio.Perfomance) + '\n')
-    #for fund in portfolio.Funds:
-    #    print('FundName: '+fund.Name)
-    #    print('SharpeRatioData: '+str(fund.SharpeRatioData))
-    #    print('Weight: '+str(fund.Weight))
-        #for stat in fund.Statistics:
-        #    for month in stat.MonthPerform:
-        #        print('Month '+str(month.Month)+' Year '+str(month.Year)+' Perf ' + str(month.Perfomance))
 
 if __name__ == "__main__":
     Main()
